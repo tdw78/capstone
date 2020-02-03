3ca4d6d2da405608ef5f3af0aa11ed3d9d1cd0d4
Tim Weiss <tdweiss1978@gmail.com>
2020-02-03 14:07:11 -0800

    User accounts complete

---
+++ spec/unit/user_spec.js
@@ -0,0 +1,51 @@
+const User = require("../../src/db/models").User;
+const sequelize = require("../../src/db/models/index").sequelize;
+
+describe("User", () => {
+
+  beforeEach((done) => {
+    sequelize.sync({force: true})
+    .then(() => {
+        done();
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+
+  describe("#create", () => {
+    it("should create a new user account", (done) => {
+        User.create({
+          name: "Timbo",
+          email: "timbo@mail.com",
+          password: "abc123"
+        })
+        .then((user) => {
+          expect(user.name).toBe("Timbo");
+          expect(user.email).toBe("timbo@mail.com");
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+    });
+
+     it("should not creat a user with input that doesn't meet validation standards", (done) => {
+         User.create({
+           name: "Timbo",
+           email: "timbo@mail.com",
+           password: "123"
+         })
+         .then((user) => {
+            done();
+         })
+         .catch((err) => {
+           expect(err.message).toContain("Validation error: must be a valid password");
+           done();
+         });
+     });
+
+  });
+});
---
+++ src/db/migrations/20200203173942-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "Must be a vaild email"}
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
