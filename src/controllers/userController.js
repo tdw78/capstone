ae6f70f62ac086fa1b328ae586c1a8248e8d522a
Tim Weiss <tdweiss1978@gmail.com>
2020-02-03 15:14:05 -0800

    Reset

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,3 @@
-const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
 const flash = require("express-flash");
 
@@ -7,45 +6,7 @@ module.exports = {
   signUpForm(req, res, next){
       res.render("users/signUpForm");
   },
-  signUp(req, res, next){
-    let newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirmation: req.body.passwordConfirmation
-    };
-    userQueries.createUser(newUser, (err, user) => {
-      if(err){
-        req.flash("error", err);
-        console.log(err);
-        res.redirect("/users/signUp");
-  
-      } else {
-  
-        passport.authenticate("local")(req, res, () => {
-          req.flash("notice", "You've successfully signed up!");
-          res.redirect("/");
-        });          
-      }
-    });
-  },
   signInForm(req, res, next){
     res.render("users/signInForm");
-  },
-  signOut(req, res, next){
-      req.logout();
-      req.flash("notice", "You have signed out");
-      res.redirect("/");
-  },
-  userSignIn(req, res, next){
-    passport.authenticate("local")(req, res, function () {
-      if(!req.user){
-        req.flash("notice", "Sign in failed. Please try again.")
-        res.redirect("/users/signIn");
-      } else {
-        req.flash("notice", "You've successfully signed in!");
-        res.redirect("/");
-      }
-    })
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
-const validation = require("./validation");
 
 router.get("/users/signUp", userController.signUpForm);
-router.post("/users", validation.validateUsers, userController.signUp);
-
 router.get("/users/signIn", userController.signInForm);
-router.post("/users/signIn", validation.validateUsers, userController.userSignIn);
-
-router.get("/users/signOut", userController.signOut);
 
 module.exports = router;
