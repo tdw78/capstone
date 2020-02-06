d69300417e3d9a32f5d73450348daf8952f960f2
Tim Weiss <tdweiss1978@gmail.com>
2020-02-06 11:03:05 -0800

    Galleries Complete

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -17,5 +17,42 @@ module.exports = {
   },
   reviews(req, res, next){
     res.render("static/reviews");
-  }
+  },
+  decorations(req, res, next){
+    res.render("albums/decorations");
+  },
+  decorSlideshow(req, res, next){
+    res.render("albums/decorSlideshow");
+  },
+  cakes(req, res, next){
+   res.render("albums/cakes");
+  },
+  cakeSlideshow(req, res, next){
+    res.render("albums/cakeSlideshow");
+   },
+   catering(req, res, next){
+    res.render("albums/catering");
+   },
+   cateringSlideshow(req, res, next){
+     res.render("albums/cateringSlideshow");
+   },
+   dresses(req, res, next){
+      res.render("albums/dresses");
+   },
+   dressSlideshow(req, res, next){
+      res.render("albums/dressSlideshow");
+    },
+   reception(req, res, next){
+      res.render("albums/reception");
+   },
+   receptionSlideshow(req, res, next){
+      res.render("albums/receptionSlideshow");
+    },
+
+   invitations(req, res, next){
+      res.render("albums/invitations");
+   },
+   inviteSlideshow(req, res, next){
+      res.render("albums/inviteSlideshow");
+   }
 }
